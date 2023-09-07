e4133e3539a574f68299564afaf2140ae2811541
koyoungsan <144293477+koyoungsan@users.noreply.github.com>
2023-09-07 10:45:37 +0900

    hello world

--- Project2/FileName.cpp
+++ Project2/FileName.cpp
@@ -1,8 +1,25 @@
 #include <stdio.h>
 
+
 int main(void)
 {
-	printf("hello world");
+	int a;
+	int b;
+	int c;
+	int d;
+	scanf_s("%d", &d);
+	printf("%d층 피라미드\n" ,d);
+	for (a = 0; a < d; a++)
+	{
+		for (b = a; b <= 5 - 1; b++) {
+			printf(" ");
+		}
+		for (c = 0; c <= (a * 2); c++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
 
 	return 0;
 }
