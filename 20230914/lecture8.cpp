f176f9277e9e53de3f8af806e881e573b846abd3
limjoonhyuk <144293477+koyoungsan@users.noreply.github.com>
2023-09-14 11:42:51 +0900

    lecture8 switch

---
+++ 20230914/lecture8.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int jumsu;
+	scanf_s("%d", &jumsu);
+
+	if (jumsu > 100 || jumsu < 0) {
+		printf("잘못입력");
+		return 0;
+	}
+	
+	{
+		int mok = jumsu / 10;
+
+		switch(mok)
+		{
+		case 10:
+		case 9:
+				printf("A");
+				break;
+		case 8:
+			printf("B");
+			break;
+		case 7:
+			printf("C");
+			break;
+		case 6:
+			printf("D");
+			break;
+		default:
+			printf("F");
+
+		}
+	}
+}
