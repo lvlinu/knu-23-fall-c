12f3b42800bbaf6774125ca0bfc231eed4b25280
lvlinu <s01085056889@gmail.com>
2023-09-21 10:47:08 +0900

    lacture2

--- Project1/practice2.cpp
+++ Project1/practice2.cpp
@@ -1,27 +1,52 @@
 #include <stdio.h>
 
+int calculator(int selector, int num1, int num2);
+int sum(int num1, int num2);
+int sub(int num1, int num2);
+int mul(int num1, int num2);
+int div(int num1, int num2);
+
 int main(void)
 {
-	int sel;
+	int sel, num1, num2;
 
-	printf("1 더하기 2빼기 3 곱하기 4 나누기\n");
+	printf("1 더하기 2빼기 3 곱하기 4 나누기 :\n");
 	scanf_s("%d", &sel);
+	printf("숫자 2개를 입력하세요(띄어쓰기로 구분)");
+	scanf_s("%d %d", &num1, &num2);
 
-	calculator(sel);
-	
+	int temp = calculator(sel, num1, num2);
+	printf("%d", temp);
 
 	return 0;
 }
-
-void calculator(int selecor)
+int calculator(int selector, int num1, int num2)
 {
-	int num1;
-	int num2;
+	if (selector < 1 || selector > 4) {
+		return 0;
+	}
+	if (selector == 1) return sum(num1, num2);
+	else if (selector == 2) return sub(num1, num2);
+	else if (selector == 3) return mul(num1, num2);
+	else if(selector == 4) return div(num1, num2);
 
-	printf("숫자 1을 입력하시오:\n");
-	scanf_s("%lf", &num1);
 
-	printf("숫자 2를 입력하시오\n");
-	scanf_s("%lf", &num2);
+}
 
+int sum(int num1, int num2)
+{
+	return num1 + num2;
+
+}
+int sub(int num1, int num2)
+{
+	return num1 - num2;
+}
+int mul(int num1, int num2)
+{
+	return num1 * num2;
+}
+int div(int num1, int num2)
+{
+	return num1 / num2;
 }
